062dd98456871278152925a6fc7f0984f56b9d54
Tim <t.wojtylak@gmail.com>
2024-07-14 14:23:30 -0400

    WIP

--- src/app/dialogs/factory-dialog/factory-dialog.component.ts
+++ src/app/dialogs/factory-dialog/factory-dialog.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
-  selector: 'app-factory-dialog',
-  standalone: true,
-  imports: [],
-  templateUrl: './factory-dialog.component.html',
-  styleUrl: './factory-dialog.component.sass'
+    selector: 'app-factory-dialog',
+    standalone: true,
+    imports: [
+        InputTextModule,
+        ButtonModule
+    ],
+    templateUrl: './factory-dialog.component.html',
+    styleUrl: './factory-dialog.component.sass'
 })
 export class FactoryDialogComponent {
+    private dynamicDialogRef = inject(DynamicDialogRef);
 
+    createFactory() {
+        this.dynamicDialogRef.close();
+    }
 }
