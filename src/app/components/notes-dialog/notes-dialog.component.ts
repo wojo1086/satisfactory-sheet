68e0fcb54b27de9c6eb1512cfff034c7f6ef8077
timothywojtylak <t.wojtylak@gmail.com>
2024-08-22 18:19:17 -0400

    Added notes modal pre-edit

--- src/app/components/notes-dialog/notes-dialog.component.ts
+++ src/app/components/notes-dialog/notes-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { EditorModule } from 'primeng/editor';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
@@ -18,13 +18,18 @@ import { FormsModule } from '@angular/forms';
 export class NotesDialogComponent {
     private ref = inject(DynamicDialogRef);
     private config = inject(DynamicDialogConfig);
-    note = this.config.data.note;
+    note = !!this.config.data.note ? this.config.data.note : 'You don\'t have any notes yet.';
+    isEditing = signal(false);
 
     cancelEditNote() {
-        this.ref.close(undefined);
+        this.isEditing.set(false);
     }
 
     saveNote() {
         this.ref.close(this.note);
+    }
+
+    editNote() {
+        this.isEditing.set(true);
     }
 }
